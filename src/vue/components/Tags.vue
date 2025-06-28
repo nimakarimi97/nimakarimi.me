39cd4e514b15224fff63926645741ca3de5c0f6e
Nima Karimi <nimakarimi676@gmail.com>
2025-06-28 13:20:45 +0200

    feat: enhance time span (badge) styling in Tags component

--- src/vue/components/Tags.vue
+++ src/vue/components/Tags.vue
@@ -47,4 +47,47 @@ const badgeClassList = computed(() => {
     margin-right: 0.3rem;
   }
 }
+
+div.badge {
+  display: inline-flex;
+  align-items: center;
+  justify-content: start;
+
+  background-color: lighten($background-color, 3%);
+  border: 1px solid rgba(black, 0.1);
+  border-radius: 10px;
+  white-space: nowrap;
+
+  padding: 4px 12px;
+  font-size: 0.8rem;
+
+  box-shadow:
+    0 2px 8px 0 rgba(0, 0, 0, 0.08),
+    0 1.5px 4px 0 rgba(0, 0, 0, 0.06);
+
+  i {
+    margin-right: 5px;
+    color: $dark;
+  }
+
+  span {
+    width: 100%;
+    text-align: center;
+  }
+
+  @include media-breakpoint-down(xl) {
+    font-size: 0.75rem;
+    padding: 3px 12px;
+  }
+
+  @include media-breakpoint-down(lg) {
+    font-size: 0.725rem;
+    padding: 3px 10px;
+  }
+
+  @include media-breakpoint-down(sm) {
+    font-size: 0.7rem;
+    padding: 3px 9px;
+  }
+}
 </style>
