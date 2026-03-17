40c788762159dec7c9ca830dbc4a3c29f31c3f04
Nima Karimi <nima.karimi@guest.telecomitalia.it>
2026-03-17 15:52:57 +0100

    feat: Configure Vite for dependency chunking and SCSS preprocessor options, and refine navigation sidebar hover effects and active link font weight.

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,35 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/',
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('vue') || id.includes('vue-router') || id.includes('vue-chartjs')) {
+              return 'vue'
+            }
+            if (
+              id.includes('bootstrap')
+              || id.includes('swiper')
+              || id.includes('chart.js')
+              || id.includes('@fortawesome')
+            ) {
+              return 'vendor'
+            }
+            return 'dependencies'
+          }
+        },
+      },
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        quietDeps: true,
+        silenceDeprecations: ['color-functions', 'import', 'global-builtin', 'legacy-js-api'],
+      },
+    },
+  },
 })
